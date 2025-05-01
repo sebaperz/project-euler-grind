7e88eab318754cccfc108276d8527ebe1c7d9dd9
sebaperz <juan.s.perez.inbox@gmail.com>
2025-04-30 22:47:42 -0500

    f12

--- 11-20/012.py
+++ 11-20/012.py
@@ -3,15 +3,12 @@
 # what is the value of the first triangle number to have over 500 divisors
 #
 
-output = 0
-while True:
-    output += 1
-    contador = output
-    divisor = 0
-    while contador > 0:
-        if output % contador == 0:
-            divisor += 1
-        contador -= 1
-    if divisor > 500:
-        break
-print(output)
+import numpy as np
+
+def npDivs(N):
+    divs = np.arange(1,int(N**0.5)+1)  # potential divisors up to √N
+    divs = divs[N % divs==0]           # divisors
+    comp = N//divs[::-1]               # complement quotients
+    return np.concatenate((divs,comp[divs[-1]==comp[0]:])) # combined
+for i in range(100,1000000):
+    print(npDivs(i))
