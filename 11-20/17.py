99ff02eed2099e14a4c922faf4625fc488ddddec
sebaperz <juan.s.perez.inbox@gmail.com>
2025-05-04 01:27:19 -0500

    17

--- 11-20/17.py
+++ 11-20/17.py
@@ -1,25 +1,31 @@
 # 17. Number Letter Counts
 # https://projecteuler.net/problem=17
 
-print(len("threehundredandforty-two"))
-
 
 def count_letters(num):
-
     total = ""
+    if num == 1000:
+        return len("onethousand")
+    antil20 = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+               "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    decena = ["", "", "twenty", "thirty", "forty",
+              "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-    unit = ["", "one", "two", "three", "four",
-            "five", "six", "seven", "eight", "nine"]
-    dec = ["", "teen", "twenty", "thirty", "forty",
-           "fifty", "sixty", "seventy", "eighty", "ninety"]
-    cen = ["hundred"]
-    mil = ["thousand"]
-    mill = ["million"]
+    # first 2 digits
+    two_digits = num % 100
+    if two_digits < 20:
+        total += antil20[two_digits]
+    else:
+        total += decena[two_digits // 10] + antil20[two_digits % 10]
 
-    if num == 11:
-        return len("eleven")
-    elif num == 12:
-        return len("twelve")
+    # hundreds
+    hundreds = num // 100
+    if hundreds > 0:
+        total += antil20[hundreds] + "hundred"
+        if two_digits > 0:
+            total += "and"
+
+    return len(total)
 
 
-trece13 = "thirteen"
+print(sum(count_letters(i) for i in range(1, 1001)))
